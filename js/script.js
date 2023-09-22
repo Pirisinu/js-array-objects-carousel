2a410c9aa89fbb63e6ab295e6e173a4787c2a139
Pirisinu <palazzialessandro2@gmail.com>
2023-09-22 15:53:03 +0200

    [Prove]text position

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ SVOLGIMENTO
 */
 //1.Richiamare i vari elementi
 //elementi
+const container = document.querySelector('.container');
 const boxImg = document.querySelector('.box-img');
 const ltlImg = document.querySelector('.ltl-img');
 
@@ -20,17 +21,39 @@ btnDown.classList.add('hide');
 let counter = 0;
 
 //2.Creare un arrey contenete le immagini
-const listImg = [
-  'img/01.webp',
-  'img/02.webp',
-  'img/03.webp',
-  'img/04.webp',
-  'img/05.webp' 
-]
+const images = [
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
+];
 //4.a
 //Sostituisco il ciclo trdizionale con un for..of
-for (let singleImg of listImg){
-  boxImg.innerHTML += `<img src="${singleImg}" class="my_img hide">`;
+for (let singleImg of images){
+  boxImg.innerHTML += `
+  <img src="${singleImg.image}" class="my_img hide">
+  `;
+  boxImg.innerHTML += `
+  <p class="p-absolute">${singleImg.title}</p>
+  `;
+  
   console.log(singleImg)
 }
 //5.
