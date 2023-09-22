6eab080358e0a3c7408fe17b1aa9430a8ea5b9d2
Pirisinu <palazzialessandro2@gmail.com>
2023-09-22 15:22:02 +0200

    [EDIT] CICLO FOR OF

--- js/script.js
+++ js/script.js
@@ -30,12 +30,7 @@ const listImg = [
 //4.a
 //Sostituisco il ciclo trdizionale con un for..of
 for (let singleImg of listImg){
-  my
   boxImg.innerHTML += `<img src="${singleImg}" class="my_img hide">`;
-  console.log(singleImg)
-  if (listImg[0]){
-    
-  }
 }
 //5.
 const imgCollection = document.getElementsByClassName('my_img');
